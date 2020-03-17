a71a293eb6ac4e0515f7e4bee847356e99876168
julien gourdon <jul.gourdon@gmail.com>
2020-03-17 10:15:27 +0100

    initial commit

---
+++ blazorWebassembly3.2Preview1/Shared/WeatherForecast.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace blazorWebassembly3._2Preview1.Shared
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public string Summary { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public City City { get; set; }
+    }
+
+    public class City
+    {
+        public string Name { get; set; }
+    }
+}
