5ea8fd3bb92188c71f4130b9e4afee4b684c315b
julien gourdon <jul.gourdon@gmail.com>
2020-04-22 08:17:15 +0200

    montee de version package

--- BlazorTest/Client/Program.cs
+++ BlazorTest/Client/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using Microsoft.Extensions.DependencyInjection;
 using BlazorTest.Shared;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Net.Http;
 
 namespace BlazorTest.Client
 {
@@ -13,7 +14,7 @@ namespace BlazorTest.Client
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.Services.AddBaseAddressHttpClient();
+            builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.RootComponents.Add<App>("app");
 
             await builder.Build().RunAsync();
